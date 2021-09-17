0b89ce5814e69f8ca88d787268d3b9557ba157a0
Fukurou Makoto <FukurouMakoto@protonmail.com>
2021-09-16 21:02:09 -0400

    added support for classless subnets

--- functions.py
+++ functions.py
@@ -2,8 +2,7 @@ import argparse
 
 
 # TODO:
-# The code needs to be refactored in order to differentiate when a CIDR or a full subnet address has been supplied.
-# In particular data_format needs to be changed.
+# We need to refactor the code to handle when non-default subnetting is being used.
 
 def num_to_binary(num):  # Converts number provided to binary
     try:
@@ -32,7 +31,7 @@ def num_to_binary(num):  # Converts number provided to binary
 
 
 def supply_subnet(subnet):  # IN PROGRESS
-    if '.' in subnet:  # Will convert accept a subnet or convert CIDR into subnet
+    if '.' in str(subnet):  # Will convert accept a subnet or convert CIDR into subnet
         return subnet
     else:
         return slash_to_subnet(subnet)
@@ -58,7 +57,7 @@ def slash_to_subnet(slash):  # Converts CIDR notation to subnet
 
 def find_network_address(address, slash):  # Finds the network address
     a = address.split('.')
-    b = slash_to_subnet(slash).split('.')
+    b = slash.split('.')
     c = []
     for i in range(0, 4):
         if b[i] == '255':
@@ -66,13 +65,15 @@ def find_network_address(address, slash):  # Finds the network address
         elif b[i] == '0':
             c.append('0')
         else:
-            c.append('1')
+            interesting_octet = b.index(b[i])
+            magic_number = get_magic_number(interesting_octet, b)
+            c.append(get_interesting_octet_value_network(a, interesting_octet, magic_number))
     return '.'.join([str(i) for i in c])
 
 
 def find_broadcast_address(ip_address, slash):  # Finds the broadcast address
-    a = ip_address.split('.')
-    b = slash_to_subnet(slash).split('.')
+    a = ip_address.split('.')  #IP Address
+    b = slash.split('.')       #Subnet Mask
     c = []
     for i in range(0, 4):
         if b[i] == '255':
@@ -80,8 +81,23 @@ def find_broadcast_address(ip_address, slash):  # Finds the broadcast address
         elif b[i] == '0':
             c.append('255')
         else:
-            c.append('1')
+            interesting_octet = b.index(b[i])
+            magic_number = get_magic_number(interesting_octet, b)
+            c.append(get_interesting_octet_value_broadcast(a, interesting_octet, magic_number))
     return '.'.join([str(i) for i in c])
+
+
+def get_magic_number(interesting_octet, subnet):
+    magic_number = 256 - int(subnet[interesting_octet])
+    return magic_number
+
+
+def get_interesting_octet_value_broadcast(ip_address, interesting_octet, magic_number):
+    return int(ip_address[interesting_octet]) + magic_number - 1
+
+
+def get_interesting_octet_value_network(ip_address, interesting_octet, magic_number):
+    return int(int(ip_address[interesting_octet]) / magic_number) * magic_number
 
 
 def find_ip_range_start(network_address):  # Finds where the IP range begins
@@ -99,10 +115,12 @@ def find_ip_range_end(broadcast_address):  # Finds where the IP range ends
 def parse_cli():
     ip_help = "xxx.xxx.xxx.xxx format"
     cidr_help = "standard cidr format minus the / (24, 16 etc.)"
+    subnet_help = "can enter full subnet mask here (255.255.255.0)"
     title = "Network-Tools"
     parser = argparse.ArgumentParser(description=title, prog=title)
     parser.add_argument('--ip', help=ip_help)
-    parser.add_argument('--cidr', type=int, help=cidr_help)
+    parser.add_argument('--cidr', help=cidr_help)
+    parser.add_argument('--subnet', help=subnet_help)
     args = parser.parse_args()
     return args
 
@@ -113,18 +131,22 @@ def get_ip_address(args):  # Returns ip address passed by user
 
 def get_subnet(args):  # Returns subnet passed by user
     # if '.' in sys.argv[2]:
-    return args.cidr
-
+    if args.subnet is None:
+        return args.cidr.strip('/')
+    elif args.cidr is None:
+        return args.subnet
+    elif args.subnet is not None and args.cidr is not None:
+        return args.subnet
 
 def data_format(ip_address, cidr):
     print(f"Supplied IP address: {ip_address}")
-    print(f"Supplied slash CIDR: {cidr}")
+    print(f"Supplied subnet/CIDR: {cidr}")
     print(f"IP address in binary: {ip_address_to_binary(ip_address)}")
-    subnet_mask = cidr  # To test new code replace cidr with supply_subnet(cidr)
-    print(f"Subnet Mask: {subnet_mask}")
+    subnet_mask = supply_subnet(cidr)  # To test new code replace cidr with supply_subnet(cidr)
+    print(f"Subnet Mask: {supply_subnet(subnet_mask)}")
     network_address = find_network_address(ip_address, subnet_mask)
     print(f"Network address: {network_address}")
-    broadcast_address = find_broadcast_address(ip_address, subnet_mask)
+    broadcast_address = find_broadcast_address(network_address, subnet_mask)
     print(f"Broadcast address: {broadcast_address}")
     ip_range_start = find_ip_range_start(network_address)
     ip_range_end = find_ip_range_end(broadcast_address)
