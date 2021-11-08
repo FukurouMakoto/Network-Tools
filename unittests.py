405e2882d74fb35fc1dc748795a66d59b1372a87
FukurouMakoto <FukurouMakoto@github.com>
2021-11-07 19:26:24 -0500

    made new tests

--- unittests.py
+++ unittests.py
@@ -3,7 +3,7 @@ import main
 
 class test_network_tools(unittest.TestCase):
 
-	def test_num_to_binary(self):
+	def test_num_to_binary(self): #Tests for num_to_binary
 		#Properly convert proper numbers	
 		self.assertEqual(main.num_to_binary(192),"11000000")
 		self.assertEqual(main.num_to_binary(168),"10101000")
@@ -32,6 +32,15 @@ class test_network_tools(unittest.TestCase):
 		self.assertEqual(main.num_to_binary("356"),"ERROR: Subnet Octets are limited to 255 bytes!")
 		self.assertEqual(main.num_to_binary("400"),"ERROR: Subnet Octets are limited to 255 bytes!")
 		self.assertEqual(main.num_to_binary("277"),"ERROR: Subnet Octets are limited to 255 bytes!")
+
+
+
+	def test_ip_address_to_binary(self):
+		self.assertEqual(main.ip_address_to_binary("192.168.1.128"), "11000000.10101000.00000001.10000000")
+		self.assertEqual(main.ip_address_to_binary("10.38.1.172"), "00001010.00100110.00000001.10101100")
+		self.assertEqual(main.ip_address_to_binary("255.255.255.0"), "11111111.11111111.11111111.00000000")
+		self.assertEqual(main.ip_address_to_binary("240.205.87.65"), "11110000.11001101.01010111.01000001")
+		self.assertEqual(main.ip_address_to_binary("102.211.10.1"), "01100110.11010011.00001010.00000001")
 
 if __name__ == "__main__":
     unittest.main()
